fc27316b64e0482b1d6e6e15417fb7c6546dd633
kanat220 <149220067+kanat220@users.noreply.github.com>
2023-12-11 00:33:22 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const clock = () => {
-    let date = new Date()
+// // setTimeout(() => {
+// //     alert('salam')
+// // }, 3000); 
+
+// // setInterval(() => {
+// //     alert('Kanat')
+// // }, 6000); 
+
+// let a = document.getElementById('a')
+// let b = document.getElementById('b')
+
+// let chaingA =  () => {
+//     a.style.color='red'
+//     a.style.transform='rotate(360deg)'
+//     a.style.transition='2s'
+//     a.style.backgroundColor='blue'
+//     document.body.style.backgroundColor='yellow'
+//     document.body.style.transition='2s'
+// }
+
+// let chaingB =  () => {
+//     b.style.color='blue'
+//     b.style.transform='rotate(360deg)'
+//     b.style.transition='25'
+//     b.style.backgroundColor='red'
+//     document.body.style.backgroundColor='green'
+//     document.body.style.transition='2s'
+// }
+
+// setTimeout(() => {
+//     chaingA()
+// }, 5000); 
+
+// setTimeout(() => {
+//     chaingB()
+// }, 8000); 
 
 
-let month_num = date.getMonth()
-let day = date.getDate()
-let hours = date.getHours()
-let minute = date.getMinutes()
-let seconds = date.getSeconds()
+// function Depart(dep1,...dep){
+//     console.log(dep1)
+//     // console.log(dep2)
+//     // console.log(dep3)
+//     console.log(dep)
+// }
 
-if(day<=9)day="0"+day
-if(hours<=9)hours="0"+hours
-if(minute<=9)minute="0"+minute
-if(seconds<=9)seconds="0"+seconds
-
-let month =["Ян","Фев","Март","Апрель","Май","Июн","Июль","Агуст","Сент","Окт","Ноя","Дек"]
-
-console.log(month[11])
-
-let date_time = "Бугун: " + day + " - " + month[month_num] + " саат "+ hours +":"+minute+":"+seconds
-
-let element = document.getElementById('time')
-element.innerHTML = date_time
-setTimeout(() => {
-    clock()
-}, 1000);
-
-
-
-}
-
-clock()
+// Depart('Программист',"Мугалим","Экономист")
